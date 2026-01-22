997ca0c362df9ce205ab169efb276c23061a74be
PXY <148702748+kcbpcc@users.noreply.github.com>
2026-01-22 22:36:20 +0530

    Update main.py

--- bp/cc/main.py
+++ bp/cc/main.py
@@ -1,11 +1,9 @@
 from fyers_login import get_fyers
 
 def main():
-    fyers = get_fyers()
-    profile = fyers.get_profile()
-
-    print("\n🚀 FYERS READY")
-    print(profile)
+    fyers = get_fyers()   # MUST return a valid object
+    print("\n🚀 FYERS OBJECT READY")
+    print(fyers.get_profile())
 
 if __name__ == "__main__":
     main()
